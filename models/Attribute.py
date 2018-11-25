2974df49a9463c00020fda7b2644f0676dd3d288
Richard Carranza <rishardcarranza@gmail.com>
2018-11-25 05:47:43 -0600

    Changes for mapping Class

---
+++ models/Attribute.py
@@ -0,0 +1,5 @@
+class Attribute:
+    def __init__(self, scope, type, name):
+        self.scope = scope
+        self.type = type
+        self.name = name
---
+++ models/Method.py
@@ -0,0 +1,5 @@
+class Method:
+    def __init__(self, scope, type, name):
+        self.scope = scope
+        self.type = type
+        self.name = name
